d299691c8f2873754716ab784dc233509499c565
Rudi <rudi.will.do.it@gmail.com>
2023-09-16 13:51:12 +0300

    start

---
+++ src/test/java/com/rsreu/rsreu/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.rsreu.rsreu;
+
+import com.rsreu.rsreu.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(MainController.class)
+public class HomeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@Test
+	public void testHomePage() throws Exception {
+		mockMvc.perform(get("/"))
+				.andExpect(status().isOk())
+				.andExpect(view().name("forward:index.html"));
+	}
+
+}
